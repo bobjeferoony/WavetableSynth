310b244fed4dcb71d64dc88b54d410e4766f58ab
bobjeferoony <bobjeferoony@users.noreply.github.com>
2026-05-16 14:25:18 +0100

    Fix JuceHeader.h includes to use quotes instead of angle brackets

--- Source/Envelope.h
+++ Source/Envelope.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <JuceHeader.h>
+#include "JuceHeader.h"
 
 class Envelope
 {
--- Source/WavetableOscillator.h
+++ Source/WavetableOscillator.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <JuceHeader.h>
+#include "JuceHeader.h"
 #include <vector>
 
 class WavetableOscillator
